6e67f0538f3830869908912222d77685c49b47cd
saidulislam <saidul.islam@gmail.com>
2021-03-28 22:03:19 -0400

    Add files via upload

---
+++ azure-function-mongo/getNotes/__init__.py
@@ -0,0 +1,23 @@
+import azure.functions as func
+import pymongo
+import os
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = os.environ['SIMongoDBConnection']
+        client = pymongo.MongoClient(url)
+        database = client['mongo-db-1']
+        collection = database['notes']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        return func.HttpResponse("Could not connect to mongodb",
+                                 status_code=400)
+
